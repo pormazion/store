984772f782b6f08d6b13627708033d54470e61a8
Pedroflp <pedroflp@ufrn.edu.com.br>
2021-01-31 08:28:27 -0300

    Add templates page

--- core/src/script/validation.js
+++ core/src/script/validation.js
@@ -1,30 +1,36 @@
 function validate(){
-    var code = document.getElementById("acess-code-input").value;
+    var input = document.getElementById("acess-code-input");
+    var code = input.value;
     var success = document.getElementById('success');
     var error = document.getElementById('error');
 
     if (code == "Jc83uW17"){
             success.style.display = "block";
+            error.style.display = "none";
             setTimeout(function(){
                 success.style.display = "none";
                 window.location.assign("https://drive.google.com/open?id=187xPHh6LqML9EV9D_3xsO0IRF99G5Rt4");
             }, 3000);
         } else if (code == "Hf43Ji06"){
             success.style.display = "block";
+            error.style.display = "none";
             setTimeout(function(){
                 success.style.display = "none";
-                window.location("https://drive.google.com/open?id=1vScMEwCK3BgPGs8drJuaU4mwWCpmt4Xa");
+                window.location.assign("https://drive.google.com/open?id=1vScMEwCK3BgPGs8drJuaU4mwWCpmt4Xa");
             }, 3000);
         } else if (code == "He23rM24"){
             success.style.display = "block";
+            error.style.display = "none";
             setTimeout(function(){
                 success.style.display = "none";
-                window.location("https://drive.google.com/open?id=1OBXF9fN3oRB2BhTGQ-VYWVdAVTdXJJRo");
+                window.location.assign("https://drive.google.com/open?id=1OBXF9fN3oRB2BhTGQ-VYWVdAVTdXJJRo");
             }, 3000);
         } else{
             error.style.display = "block";
+            input.style.border = '2px solid red';
             setTimeout(function(){
                 error.style.display = "none";
+                input.style.border = '2px solid transparent';
             }, 3000);
         }
     }
