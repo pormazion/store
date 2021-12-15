03bd5a9cf480dd58728c6c22d9929584c6e5ae0f
pedroflp <pedrofelipe0940@gmail.com>
2021-12-15 13:33:26 -0300

    feat: auto download by email purchased

--- core/src/script/validation.js
+++ core/src/script/validation.js
@@ -4,34 +4,12 @@ function validate(){
     var success = document.getElementById('success');
     var error = document.getElementById('error');
 
-    if (code == "Jc83uW17"){
-            success.style.display = "block";
-            error.style.display = "none";
-            setTimeout(function(){
-                success.style.display = "none";
-                window.location.assign("https://drive.google.com/open?id=187xPHh6LqML9EV9D_3xsO0IRF99G5Rt4");
-            }, 3000);
-        } else if (code == "Hf43Ji06"){
-            success.style.display = "block";
-            error.style.display = "none";
-            setTimeout(function(){
-                success.style.display = "none";
-                window.location.assign("https://drive.google.com/open?id=1vScMEwCK3BgPGs8drJuaU4mwWCpmt4Xa");
-            }, 3000);
-        } else if (code == "He23rM24"){
-            success.style.display = "block";
-            error.style.display = "none";
-            setTimeout(function(){
-                success.style.display = "none";
-                window.location.assign("https://drive.google.com/open?id=1OBXF9fN3oRB2BhTGQ-VYWVdAVTdXJJRo");
-            }, 3000);
-        } else{
-            error.style.display = "block";
-            input.style.border = '2px solid red';
-            setTimeout(function(){
-                error.style.display = "none";
-                input.style.border = '2px solid transparent';
-            }, 3000);
-        }
+    if (code === localStorage.getItem('ea91af8f9f7b35bf466115ee732177d7d3abd6df')){
+        success.style.display = "block";
+        error.style.display = "none";
+        setTimeout(function(){
+            success.style.display = "none";
+            window.location.assign("https://drive.google.com/open?id=187xPHh6LqML9EV9D_3xsO0IRF99G5Rt4");
+        }, 3000)
     }
-                    
+}             
